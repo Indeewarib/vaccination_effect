46408d6acb79758c9c8d568f0214dcc8de1ddc6b
Indeewarib <dhamithani97@gmail.com>
2024-01-19 23:36:04 +0100

    clean data

--- vaccination_effect.py
+++ vaccination_effect.py
@@ -1,6 +1,7 @@
 #===================
 # Import Libraries
 #===================
+import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -55,9 +56,12 @@ def add_header(text):
 # Main
 #===================
 
-#Read and check datasets
-infectious_diseases_df = pd.read_csv("number_of_cases_of_infectious_diseases.csv")
-vaccination_df = pd.read_csv("number_of_vaccinated_one_year_olds.csv")
+#---------------------------Read and explore datasets-----------------------------------------------------
+full_path = os.path.realpath(__file__)
+project_folder = os.path.dirname(full_path) 
+
+infectious_diseases_df = pd.read_csv(f'{project_folder}\\data\\number_of_cases_of_infectious_diseases.csv')
+vaccination_df = pd.read_csv(f'{project_folder}\\data\\number_of_vaccinated_one_year_olds.csv')
 
 infectious_diseases_df.info()
 infectious_diseases_df.describe()
@@ -66,3 +70,25 @@ infectious_diseases_df.head()
 vaccination_df.info()
 vaccination_df.describe()
 vaccination_df.head()
+
+#---------------------------Clean datasets----------------------------------------------------------------
+
+#Drop the 'Code' columns from both dataframes
+infectious_diseases_df.drop(['Code'], axis = 1, inplace = True)
+vaccination_df.drop(['Code'], axis = 1, inplace = True)
+
+#Change column names in both dataframes
+infectious_diseases_df.columns = ['country','year','yaws_cases','polio_cases','guinea_worm_disease_cases','rabies_cases','malaria_cases','HIV_AIDS_cases','tuberculosis_cases','smallpox_cases','cholera_cases']
+vaccination_df.columns = ['country','year','HeoB3','DTP','polio','population','measles','Hib3','rubella','rotavirus','BCG']
+
+#In both dataframes, fill missing values with 0
+infectious_diseases_df.fillna(0, inplace = True)
+vaccination_df.fillna(0, inplace = True)
+
+#Check dataframes
+infectious_diseases_df.info()
+infectious_diseases_df.head()
+
+vaccination_df.info()
+vaccination_df.head()
+
